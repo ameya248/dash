e2e99fda86905ee3c4805bdf99732b524b42e956
ameya248 <110188899+ameya248@users.noreply.github.com>
2023-03-23 15:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 st.title("Water Analysis")
 
 #add a sidebar
 st.sidebar.subheader("Visualisation Settings")
-dataset=st.beta_container()
-with dataset:
+#dataset=st.beta_container()
+#with dataset:
 
-    st.header('lake dataset')
+    #st.header('lake dataset')
 
-    HK_data = pd.read_csv('finalHK.csv')
+    #HK_data = pd.read_csv('finalHK.csv')
 
-    st.write(HK_data.head())
+    #st.write(HK_data.head())

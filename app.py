e1e12232aacc3696aa37729ddb3edfa0facbb50b
ameya248 <110188899+ameya248@users.noreply.github.com>
2023-03-24 15:12:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,52 @@ import chart_studio.tools as tls
 import plotly.graph_objs as go
 
 
-with st.sidebar:
-    selected = option_menu.option_menu(
-        menu_title = "Main Menu",
-        options = ["Home", "EDA"]
-    )
-#add a sidebar
-st.sidebar.subheader("Visualisation Settings")
-dataset=st.beta_container()
-with dataset:
 
-    st.header('lake dataset')
+    
 
-    HK_data = pd.read_csv('finalHK.csv')
+# add a select widget to the sidebar
+chart_select = st.sidebar.selectbox(
+    label = "Select the Chart Type",
+    options = ['Scatterplots', 'Lineplots', 'Histogram', 'Boxplot']
+)
 
-    st.write(HK_data)
+if chart_select == 'Scatterplots':
+    st.sidebar.subheader('Scatterplot Settings')
+    try:
+        x_values = st.sidebar.selectbox('X axis', options = numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options = numeric_columns)
+        plot = px.scatter(data_frame = df, x = x_values, y = y_values)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Lineplots':
+    st.sidebar.subheader('Lineplots Settings')
+    try:
+        x_values = st.sidebar.selectbox('X axis', options = numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options = numeric_columns)
+        plot = px.area(data_frame = df, x = x_values, y = y_values)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Boxplot':
+    st.sidebar.subheader('Boxplot Settings')
+    try:
+        x_values = st.sidebar.selectbox('X axis', options = numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options = numeric_columns)
+        plot = px.box(data_frame = df, x = x_values, y = y_values)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Histogram':
+    st.sidebar.subheader('Histogram Settings')
+    try:
+        x_values = st.sidebar.selectbox('Select the variable to plot histogram', options = numeric_columns)
+        bins = st.sidebar.slider("Select the number of bins", min_value=5, max_value=50, value=20, step=1)
+        plot = px.histogram(data_frame = df, x = x_values, nbins=bins)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+

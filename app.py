917bae46c598821d0c6257d0c4521acd36891a02
Ananya1301 <110127399+Ananya1301@users.noreply.github.com>
2023-03-25 16:32:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,28 @@ import pandas as pd
 import plotly.express as px
 import streamlit_option_menu as option_menu
 
-<styles>
-.waterQuality{
- font-size: 30px;
+st.markdown(
+  """
+  <styles>
+  .waterQuality{
+  font-size: 30px;
   font-family: "Source Sans Pro", sans-serif;
   font-weight: 600;
   color: #393053;
-}
-</styles>
+  }
+  </styles>
+  """,
+  unsafe_allow_html = True
+)
 
-# with open('styles.css')as f:
-#  st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 #title
-st.markdown("Water Quality Analysis of Bhopal Lakes", unsafe_allow_html = True, attributes = {"class": "waterQuality"})
+st.markdown(
+ f"""
+ <div class = "waterQuality"> "Water Quality Analysis of Bhopal Lakes" <br> </div>
+ """, 
+ unsafe_allow_html = True
+ )
 
 #add a sidebar
 st.sidebar.subheader("Visualisation Settings")
